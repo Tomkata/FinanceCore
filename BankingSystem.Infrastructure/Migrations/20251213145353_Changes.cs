f7d881f14b5cbeb9cfca183190c88da6e7ee60d9
Toma Andreev <tomaanreev12@gmail.com>
2025-12-13 16:54:29 +0200

    Migrations

---
+++ BankingSystem.Infrastructure/Migrations/20251213145353_Changes.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_IdempotencyKey",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IdempotencyKey",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Transactions",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[] { 0 },
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AccountId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "TransactionEntries",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[] { 0 },
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Customers",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[] { 0 },
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Accounts",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[] { 0 },
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Transactions",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true,
+                oldDefaultValue: new byte[] { 0 });
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AccountId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdempotencyKey",
+                table: "Transactions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "TransactionEntries",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true,
+                oldDefaultValue: new byte[] { 0 });
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Customers",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true,
+                oldDefaultValue: new byte[] { 0 });
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "Accounts",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "rowversion",
+                oldRowVersion: true,
+                oldDefaultValue: new byte[] { 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_IdempotencyKey",
+                table: "Transactions",
+                column: "IdempotencyKey",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
