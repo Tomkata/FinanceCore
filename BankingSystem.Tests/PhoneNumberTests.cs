de1590e18438ac0ade285253a7dd4bc053d9007f
Toma Andreev <tomaanreev12@gmail.com>
2025-12-13 16:17:11 +0200

    PhoneNumberTests

---
+++ BankingSystem.Tests/PhoneNumberTests.cs
@@ -0,0 +1,64 @@
+﻿using BankingSystem.Domain.Exceptions;
+using BankingSystem.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace BankingSystem.Tests;
+
+public class PhoneNumberTests
+{
+    [Theory]
+    [InlineData("+359888123456")]
+    [InlineData("+1234567890")]
+    [InlineData("0888123456")]
+    [InlineData("+359123456789012345")] // 15 digits max
+    public void Create_WithValidPhoneNumber_ShouldSucceed(string phoneNumber)
+    {
+        // Act
+        var result = new PhoneNumber(phoneNumber);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Value.Should().Be(phoneNumber);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("123")] // Too short (less than 10 digits)
+    [InlineData("+35988812345abc")] // Contains letters
+    [InlineData("088 812 3456")] // Contains spaces
+    [InlineData("+3598881234567890123")] // Too long (more than 15 digits)
+    public void Create_WithInvalidPhoneNumber_ShouldThrowInvalidPhoneNumberException(string phoneNumber)
+    {
+        // Act
+        var act = () => new PhoneNumber(phoneNumber);
+
+        // Assert
+        act.Should().Throw<InvalidPhoneNumberException>();
+    }
+
+    [Fact]
+    public void PhoneNumbers_WithSameValue_ShouldBeEqual()
+    {
+        // Arrange
+        var phone1 = new PhoneNumber("+359888123456");
+        var phone2 = new PhoneNumber("+359888123456");
+
+        // Assert
+        phone1.Should().Be(phone2);
+        (phone1 == phone2).Should().BeTrue();
+    }
+
+    [Fact]
+    public void PhoneNumbers_WithDifferentValues_ShouldNotBeEqual()
+    {
+        // Arrange
+        var phone1 = new PhoneNumber("+359888123456");
+        var phone2 = new PhoneNumber("+359888654321");
+
+        // Assert
+        phone1.Should().NotBe(phone2);
+        (phone1 != phone2).Should().BeTrue();
+    }
+}
